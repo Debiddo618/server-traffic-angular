2640c178dc07bdbf21858c7d92819b6d2cf0677d
Debiddo618 <dzheng@terpmail.umd.edu>
2025-05-08 13:37:43 -0700

    changed CSS and used @HostBinding to listen to events

---
+++ src/app/shared/control/control.component.ts
@@ -0,0 +1,35 @@
+import {
+  Component,
+  ElementRef,
+  HostBinding,
+  HostListener,
+  inject,
+  input,
+  ViewEncapsulation,
+} from '@angular/core';
+
+@Component({
+  selector: 'app-control',
+  standalone: true,
+  imports: [],
+  templateUrl: './control.component.html',
+  styleUrl: './control.component.css',
+  encapsulation: ViewEncapsulation.None,
+  host: {
+    class: 'control',
+    // '(click)': 'onClick()'
+  },
+})
+export class ControlComponent {
+  private el = inject(ElementRef);
+  // @HostBinding('class') className = 'control';
+  @HostListener('click') onClick() {
+    console.log('Clicked');
+    console.log(this.el);
+  }
+  label = input.required<string>();
+
+  // onClick = () => {
+  //   console.log("clicked");
+  // }
+}
